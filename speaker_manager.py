b59fdd2773649fbf63e5429aa9c53c921e7d3ec0
selligZeittom <gilmottiez@gmail.com>
2019-01-18 09:36:50 +0100

    now report time

--- speaker_manager.py
+++ speaker_manager.py
@@ -7,7 +7,7 @@ class TTS3:
     def __init__(self):
         self.engine = pyttsx3.init()
         volume = self.engine.getProperty('volume')
-        self.engine.setProperty('volume', volume + 0.25)
+        self.engine.setProperty('volume', volume + 0.75)
         rate = self.engine.getProperty('rate')
         self.engine.setProperty('rate', rate-25)
         voice = self.engine.getProperty('voices')[26]  # the french voice
